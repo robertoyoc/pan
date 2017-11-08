a2940db12fa8551cdccfc76f4dff29aafdab0437
robertoyoc <a01172599@itesm.mx>
2017-11-08 10:10:28 -0600

    carrito funcional

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,4 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-
-  prueba: 1,
-  actions: {
-    add(){
-      this.set('prueba', this.get('prueba')+1)
-    }
-  }
 });
