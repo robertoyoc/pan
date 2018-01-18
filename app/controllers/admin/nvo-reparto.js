b8cecaca60d5d5ee7f9c599defeeb920c4e01247
Roberto Yoc <azroberto0924@gmail.com>
2018-01-17 20:51:10 -0600

    interfaz nvo reparto terminada

--- app/controllers/admin/nvo-reparto.js
+++ app/controllers/admin/nvo-reparto.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+	disabledAgregar: computed('cantidad', function () {
+		return Ember.isBlank(this.get('cantidad'));
+	}),
 	actions: {
 		foo(){
 			
@@ -13,10 +17,14 @@ export default Controller.extend({
 				producto: producto,
 				cantidad: cantidad
 			})
+			this.set('cantidad', undefined)
 
 		},
 		delete(distribucion){
 			distribucion.destroyRecord()
+		}, 
+		enviar(){
+
 		}
 	}
 });
