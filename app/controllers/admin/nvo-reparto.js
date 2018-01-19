b5268a41da3ecc92512b7b8b5064f8c5618213f3
Roberto Yoc <azroberto0924@gmail.com>
2018-01-19 14:35:30 -0600

    WIP

--- app/controllers/admin/nvo-reparto.js
+++ app/controllers/admin/nvo-reparto.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { validator, buildValidations } from 'ember-cp-validations';
+import { all } from 'rsvp'
 
 const Validations = buildValidations({
 	'cantidad': [
@@ -22,6 +23,15 @@ export default Controller.extend(Validations, {
 	cantidadMax: computed('selectedProducto', function(){
 		return this.get('selectedProducto.cantidad')
 	}),
+	plural: computed('cantidad', function(){
+		return this.get('cantidad')>1
+	}),
+	valid: computed('cantidad', function () {
+		return (this.get('validations.attrs.cantidad.isValid')||this.get('disabledAgregar'))?'validate': 'validate invalid';
+	}),
+	disabledEnviar: computed('model.reparto.distribuciones', function() {
+		return this.get('model.reparto.distribuciones.length')>0
+	}),
 	actions: {
 		foo(){
 			
@@ -40,7 +50,23 @@ export default Controller.extend(Validations, {
 		delete(distribucion){
 			distribucion.destroyRecord()
 		}, 
-		enviar(){
+		enviar(reparto){
+			let date = Date.now()
+			let formatDate= Date.parse(date);
+			reparto.set('fecha', date)
+			let day = formatDate.getDate();
+			let month = formatDate.getMonth()+1;
+			let year = formatDate.getFullYear();
+
+			let emberDate= year + month + year;
+
+			// all(reparto.get('distribuciones').invoke('save')).then(()=>{
+			// 	reparto.save();
+			// }).then(()=>{
+			// 	window.swal("Guardado!", "El reparto ha sido enviado!", "success");
+			// }).then(()=>{
+			// 	this.transitionToRoute('admin.inv-produccion')
+			// })
 
 		}
 	}
--- app/models/reparto.js
+++ app/models/reparto.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	sucursal: DS.belongsTo('sucursal'),
 	distribuciones: DS.hasMany('distribucion'),
-	fecha: DS.attr('date')
+	fecha: DS.attr('string'),
+	dia: DS.attr('date'),
+	hora: DS.attr('string')
 });
