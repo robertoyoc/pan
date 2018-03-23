83a1b6625a343b8a5c7a81f2186258e9c47b60e8
lex_rdz <aleerdzmtz15@hotmail.com>
2018-03-23 16:18:35 -0600

    Boton de venta finalizado

--- app/components/venta-vendedor.js
+++ app/components/venta-vendedor.js
@@ -1,9 +1,13 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { inject as service } from "@ember/service";
 import { all } from 'rsvp';
 
 export default Component.extend({
-    
+    disabledVender: computed('myModel', function() {
+		return this.get('myModel.pedidos.length') > 0;
+    }),
+
     actions: {
         delete(pedido){
 			pedido.destroyRecord()
