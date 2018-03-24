8f1bb62d9939ed5e9cfe1ca19c667b53c0e3c5c7
lex_rdz <aleerdzmtz15@hotmail.com>
2018-03-23 21:24:04 -0600

    Avance component productos/nuevareceta

--- app/models/maquila.js
+++ app/models/maquila.js
@@ -1,11 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-<<<<<<< HEAD
 	receta: DS.belongsTo('receta'),
-=======
-	receta: DS.hasMany('receta'),
->>>>>>> f056ac9e78827edf7f2f4c8e5453ac145b9563c1
 	cantidad: DS.attr('number'),
 	produccion: DS.belongsTo('produccion')
 });
