ab5c3d6651f351b6c1f7b3a188f8547c563f04ee
lex_rdz <aleerdzmtz15@hotmail.com>
2018-04-11 14:00:28 -0500

    Avance filtro-reparto

--- app/components/filtro-reparto.js
+++ app/components/filtro-reparto.js
@@ -1,4 +1,17 @@
 import Component from '@ember/component';
+import moment from 'moment';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+    month: null,
+    day: null,
+    
+    daysInMonth: computed('month',function(){
+		if(!this.get('year') || !this.get('month') || this.get('month') == '-1'){ return [] }
+
+		let monthObj = moment([this.get('year'), Number(this.get('month')) - 1, 1]);
+		let daysInMonth = monthObj.daysInMonth();
+
+		return Array.from(Array(daysInMonth), (_,x) => `${x + 1}`);
+	})
 });
