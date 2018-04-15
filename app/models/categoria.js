84c800933e210e1475a475fa7d9a09a0e591e5a6
lex_rdz <aleerdzmtz15@gmail.com>
2018-04-15 18:09:52 -0500

    Correccion en modelo categorias

--- app/models/categoria.js
+++ app/models/categoria.js
@@ -1,7 +1,23 @@
 import { computed } from '@ember/object';
 import DS from 'ember-data';
-
+import {all} from 'rsvp' 
 
 export default DS.Model.extend({
-  nombre: DS.attr('string')
+  	nombre: DS.attr('string'),
+
+  	productosId: DS.attr({
+		defaultValue: function(){
+			return []
+		}
+	}),
+
+	productos: computed('productosId', function(){
+		return all(
+			this.get('productosId').map((producto)=>{
+				let id = producto.id
+				let path  =producto.tipo
+				return this.store.findRecord(path, id)
+			})
+		)
+	})
 });
