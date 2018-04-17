2decb6ae414a39c03b6e24e96f4ce5ecb04909ab
Roberto Yoc <azroberto0924@gmail.com>
2018-04-17 00:28:54 -0500

    fixed bug to find

--- app/models/pedido.js
+++ app/models/pedido.js
@@ -13,7 +13,9 @@ export default DS.Model.extend({
   	tipo: DS.attr('string'),
 	  productoId: DS.attr('string'),
 	  producto: computed('tipo', 'productoId', function(){
-		  return this.store.findRecord(this.get('tipo'), this.get('productoId'))
+      if(this.get('tipo'))
+		    return this.store.findRecord(this.get('tipo'), this.get('productoId'))
+      else return null
 	  }),
 
 });
