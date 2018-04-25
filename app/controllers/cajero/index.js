a286609c5b7781bdbeb6831eb58daeb9cf25d007
serivico01 <serivico01@sldid03-3.local>
2018-04-24 21:09:15 -0500

    Avance en cajero/index

--- app/controllers/cajero/index.js
+++ app/controllers/cajero/index.js
@@ -1,17 +1,18 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-
+import moment from 'moment';
 import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 
 export default Controller.extend(FindQuery, {
+	result: null,
 	names: ['Efectivo', 'Tarjeta Debito/Credito', 'Cheque',],
 
 	selectedVenta: computed('result', function(){
 		if(this.get('result'))
 		{
-			console.log(this.get('result'))
+			//console.log(this.get('result'))
 			let venta = this.store.findRecord('venta', this.get('result'))
-			console.log(venta)
+			//console.log(venta)
 			if(!venta.get('fechaPago')){
 				return venta;
 			} else {
@@ -22,11 +23,25 @@ export default Controller.extend(FindQuery, {
 		}
 	}),
 
+	disabledPagar: computed('pagoCliente', 'selectedVenta', function() {
+		return Number(this.get('pagoCliente')) >= Number(this.get('selectedVenta.importeTotal'));
+    }),
+
 	cambioCliente: computed('pagoCliente', 'selectedVenta', function(){
 		if(this.get('selectedVenta.importeTotal')){
-			return Number(this.get('pagoCliente')) - Number(this.get('selectedVenta.importeTotal'))
+			if (Number(this.get('pagoCliente')) >= Number(this.get('selectedVenta.importeTotal')))
+				return Number(this.get('pagoCliente')) - Number(this.get('selectedVenta.importeTotal'))
+			else
+				return 0
 		} else {
 			return 0;
 		}
-	})
+	}),
+
+	actions: {
+		realizarPago(venta){
+			venta.set('fechaPago', moment().format())
+			this.set('result', null);
+		}
+	}
 });
