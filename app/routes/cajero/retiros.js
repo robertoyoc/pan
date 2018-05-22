08f57b08e107d8e2b07e35edba229eeb3029b5c9
Alex Resendiz <aleerdzmtz15@hotmail.com>
2018-05-21 19:35:30 -0500

    Filtros agregados en Cajero/Historial, Cajero/Retiros.  Tickets de Venta Funcionales

--- app/routes/cajero/retiros.js
+++ app/routes/cajero/retiros.js
@@ -3,27 +3,52 @@ import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 import { inject as service } from "@ember/service";
 import moment from 'moment';
 
-
-
 export default Route.extend(FindQuery, {
   currentUser: service(),
+
+  queryParams: {
+    date: {
+      refreshModel: true
+    },
+    isToday: {
+      refreshModel: true
+    }
+  },
+
+  dateFromParams(params) {
+    let fechaUnix = (params.isToday == 'true') ? moment.unix(params.date) : moment(Number(params.date));
+    return moment(fechaUnix);
+  },
+
   beforeModel() {
 
   },
-  model() {
+
+  model(params) {
+    let dateFromParams = this.dateFromParams(params),
+      startDate, endDate;
+    startDate = dateFromParams.clone().startOf('day').utc();
+    endDate = dateFromParams.clone().endOf('day').utc();
+
+
     return this.get('currentUser.account').then((account) => {
       this.set('accountId', account.get('id'))
       let sucursal = account.get('sucursal');
       this.set('sucursalId', sucursal.get('id'))
 
       let context = this;
-      // debugger
       return new Promise(function(resolve, reject) {
-        // debugger
-        context.filterEqual(context.store, 'retiro', { 'fechaShort': "8 May, 2018", 'sucursalId': sucursal.get('id') }, function(recetas) {
-          // console.log(recetas)
-          return resolve(recetas)
-
+        context.filterCustom(context.store, 'retiro', { 
+          'sucursalId': ['==', sucursal.get('id')],
+          'fechaUnix': ['>=', startDate.unix()]
+        }, function(retiros) {
+          let retirosList = [];
+            retiros.forEach(function(retiro){
+              if (retiro.get('fechaUnix') <= endDate.unix()) {
+                retirosList.pushObject(retiro);
+              }
+            })
+            return resolve(retirosList)
         })
       })
     })
